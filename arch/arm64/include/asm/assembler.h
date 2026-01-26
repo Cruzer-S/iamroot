f24dec9e4ef5d011e9218f2c958df6fd0b3481ee
Taewan Kim <runixs92@gmail.com>
2026-01-26 23:38:32 +0900

    arm64: modify 261024 comments
    Update comments based on feedback from Sang-Heon Jeon (hqsz).
    
    Link: https://github.com/hqsz/iamroot/pull/3
    
    Suggested-by: Sang-Heon Jeon <ekffu200098@gmail.com>
    Signed-off-by: Taewan Kim <runixs92@gmail.com>

--- arch/arm64/include/asm/assembler.h
+++ arch/arm64/include/asm/assembler.h
@@ -347,7 +347,9 @@ alternative_cb_end
  *	pos:		IPS or PS bitfield position
  *	tmp{0,1}:	temporary registers
  */
-	.macro	tcr_compute_pa_size, tcr, pos, tmp0, tmp1  //tc,  #TCR_IPS_SHIFT x5, x6
+	// passed arguments are
+	//	tcr = x16,  pos = #TCR_IPS_SHIFT, tmp0 = x5, tmp1 = x6
+	.macro	tcr_compute_pa_size, tcr, pos, tmp0, tmp1
 	mrs	\tmp0, ID_AA64MMFR0_EL1  // x5
 	// Narrow PARange to fit the PS field in TCR_ELx
 	ubfx	\tmp0, \tmp0, #ID_AA64MMFR0_EL1_PARANGE_SHIFT, #3
@@ -455,7 +457,7 @@ alternative_endif
  * pgtbl preserved
  * tmp1/tmp2 clobbered, either may overlap with pgtbl
  */
-	//load_ttbr1 x1, x1, x3
+	// passed arguments from head.S are pgtbl = x1, tmp1 = x1, tmp2 = x3
 	.macro		load_ttbr1, pgtbl, tmp1, tmp2
 	phys_to_ttbr	\tmp1, \pgtbl
 	offset_ttbr1 	\tmp1, \tmp2
